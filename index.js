2022b68ccac89b5f331ff436f777f5869955f439
Julio Quiroz <chulini@gmail.com>
2020-01-05 08:54:40 +0100

    new resolution, update fix

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 const WebSocket = require('ws');
-const gridResolution = 13;
+const gridResolution = 21;
 let miningBlockHeight = -1;
 let blocksColors = []
 // i = 0 ==> block being mined
 // i = 1 ==> last block on blockchain
 for (let i = 0; i < 7; i++) {
+    const blockBg =  Math.floor(Math.random() * 16);
+
     blocksColors[i] = [];
     for (let x = 0; x < gridResolution; x++) {
         blocksColors[i][x] = [];
         for (let y = 0; y < gridResolution; y++) {
-            blocksColors[i][x][y] = (x % 2 == y % 2) ? 7 : 6;
+            blocksColors[i][x][y] = blockBg;//(x % 2 == y % 2) ? 7 : 6;
         }
     }
 }
 
 const wss = new WebSocket.Server({ port: 5000 });
-let wsc = new WebSocket('wss://ws.blockchain.info/inv');
-let wssConnected = null;
+let wsBlockTick = new WebSocket('wss://ws.blockchain.info/inv');
 
-wsc.on('open', function open() {
-    wsc.send(JSON.stringify({ op: "blocks_sub" }));
-    wsc.send(JSON.stringify({ op: "ping_block" }));
+wsBlockTick.on('open', function open() {
+    wsBlockTick.send(JSON.stringify({ op: "blocks_sub" }));
+    wsBlockTick.send(JSON.stringify({ op: "ping_block" }));
     setInterval(() => {
-        wsc.send(JSON.stringify({ op: "ping_block" }));
-
+        wsBlockTick.send(JSON.stringify({ op: "ping_block" }));
     }, 15000);
 
-
     wss.on('connection', function connection(ws) {
-        wssConnected = ws;
         ws.send('allcolors|' + JSON.stringify(blocksColors)+'|'+miningBlockHeight);
-
         ws.on('message', function incoming(data) {
             let msgSplit = data.split('|');
             if (msgSplit[0] === 'colorchange') {
@@ -38,51 +35,54 @@ wsc.on('open', function open() {
                 const y = parseInt(msgSplit[2]);
                 const newColor = parseInt(msgSplit[3]);
                 blocksColors[0][x][y] = newColor;
-
                 wss.clients.forEach(function each(client) {
                     if (client.readyState === WebSocket.OPEN) {
                         client.send(`colorchange|${x}|${y}|${blocksColors[0][x][y]}`);
                     }
                 });
             }
-
-
         });
     });
-
-    // setInterval(() => {
-    //     miningBlockHeight++;
-    //     NewBlock();
-    // }, 10000);
-
 });
 
-wsc.on('message', function incoming(data) {
+// To simulate new blocks
+// NewBlock(miningBlockHeight+1);
+// setInterval(() => {
+//     // miningBlockHeight++;
+//     NewBlock(miningBlockHeight+1);
+// }, 10000);
+
+wsBlockTick.on('message', function incoming(data) {
     let blockData = JSON.parse(data);
-    if (miningBlockHeight != blockData.x.height + 1) {
-        miningBlockHeight = blockData.x.height + 1;
-        NewBlock();
-    }
+    NewBlock(blockData.x.height);
 });
 
 
-function NewBlock() {
+function NewBlock(height) {
+    if (miningBlockHeight == height + 1)
+        return;
 
+    miningBlockHeight = height+1;
+    
     console.log(`mining new block ${miningBlockHeight}`);
     //Shift colors
     blocksColors.pop();
     let newBlockColor = [];
+    const blockBg =  Math.floor(Math.random() * 16);
+
     for (let x = 0; x < gridResolution; x++) {
         newBlockColor[x] = [];
         for (let y = 0; y < gridResolution; y++) {
-            newBlockColor[x][y] = (x % 2 == y % 2) ? 7 : 6;
+            newBlockColor[x][y] = blockBg;//(x % 2 == y % 2) ? 7 : 6;
         }
     }
     blocksColors.unshift(newBlockColor);
 
-    if (wssConnected != null) {
-        wssConnected.send('allcolors|' + JSON.stringify(blocksColors) + '|' + miningBlockHeight);
-    }
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send('allcolors|' + JSON.stringify(blocksColors) + '|' + miningBlockHeight);
+        }
+    });
 }
 
 
